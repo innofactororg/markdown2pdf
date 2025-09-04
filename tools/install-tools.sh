45188b1e565c951f1380d59b9f5c5cd1beee6dd1
Damian Flynn <info@damianflynn.com>
2025-09-04 07:57:34 +0100

    fix: chromium
    Add installation of Chromium and its dependencies to install-tools.sh.

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -8,6 +8,9 @@ aptinstall() {
   sudo apt-get install --assume-yes --no-install-recommends "${1}"
 }
 if type apt-get > /dev/null 2>&1; then
+  # Install Chromium and dependencies for Mermaid CLI (mmdc)
+  aptinstall chromium-browser || aptinstall chromium
+  aptinstall fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libcups2 libdbus-1-3 libgdk-pixbuf2.0-0 libnspr4 libnss3 libx11-xcb1 libxcomposite1 libxdamage1 libxrandr2 libgbm1 libpango-1.0-0 libpangocairo-1.0-0 libu2f-udev libvulkan1 libxss1 libxtst6
   texlivebin=''
   export DEBIAN_FRONTEND=noninteractive
   apt_update=0
@@ -63,6 +66,8 @@ if type apt-get > /dev/null 2>&1; then
   fi
   sudo npm install -g @mermaid-js/mermaid-cli
 elif type apk > /dev/null 2>&1; then
+  # Install Chromium and dependencies for Mermaid CLI (mmdc)
+  apk add --no-cache chromium
   # Install Mermaid CLI (mmdc) for Mermaid diagram rendering
   if ! type npm > /dev/null 2>&1; then
     apk add --no-cache nodejs npm
